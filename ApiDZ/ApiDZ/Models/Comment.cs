cff7477afd05f2b85aec47ddeda94c99a2f6d41f
ST_DOMAIN\Kalinevich_RS <Kalinevich_RS@st.by>
2021-09-04 11:04:00 +0300

    Add model Comment

---
+++ ApiDZ/ApiDZ/Models/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiDZ.Models
+{
+    public class Comment
+    {
+        public Guid Id { get; set; }
+        public string Category { get; set; }
+        public string ProductName { get; set; }
+        public int Rating { get; set; }
+        public string CommentText { get; set; }
+        public string Img { get; set; }
+        public string Video { get; set; }
+    }
+}
